99e32338e01e3d33ed52616e07bca69e7ef0e0d9
Amirkaramat <amir_karamat@yahoo.com>
2023-06-15 14:58:58 +0330

    FIX2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { useRoutes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Topbar from "./components/topbar/Topbar";
-import routs from "./routes/route.jsX";
+import routs from "./route";
 import SearchBox from "./components/searchBox/SearchBox";
 
 function App() {
