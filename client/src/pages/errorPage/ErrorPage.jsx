9d4534642267ee5cc741352359bf7b0b195e4a65
Amirkaramat <amir_karamat@yahoo.com>
2023-06-15 15:19:52 +0330

    fix image

--- client/src/pages/errorPage/ErrorPage.jsx
+++ client/src/pages/errorPage/ErrorPage.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function ErrorPage() {
   return (
-    <div className='bg-[url("../../public/404.jpg")] h-screen min-h-[600px] max-h-full w-screen bg-center  bg-contain bg-no-repeat bg-[#FFD105]'></div>
+    <div className='bg-[url("/src/404.jpg")] h-screen min-h-[600px] max-h-full w-screen bg-center  bg-contain bg-no-repeat bg-[#FFD105]'></div>
   );
 }
