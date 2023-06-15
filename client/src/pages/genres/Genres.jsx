7e3f3c2b3accc79e0d707708cef7289b29fa72a4
Amirkaramat <amir_karamat@yahoo.com>
2023-06-15 21:28:45 +0330

    fix 3

--- client/src/pages/genres/Genres.jsx
+++ client/src/pages/genres/Genres.jsx
@@ -11,19 +11,19 @@ export default function Genres() {
   return (
     <>
       {genresData && (
-        <>
-          <div className="grid grid-cols-2 gap-2 px-1  sm:grid-cols-3 lg:grid-cols-4 justify-center w-full text-primary text-xl">
+        <div className="flex flex-col justify-center items-center pt-[100px] px-1 sm:px-[100px] w-full">
+          <h1 className='text-primary text-2xl pb-[50px]'>Genres</h1>
+          <div className='grid grid-cols-2 gap-1  sm:grid-cols-3 lg:grid-cols-4 justify-center items-center w-full text-primary text-lg'>
             {genresData.map((genre) => (
               <NavLink
                 to={`/searchMovie/${genre.name}/${genre.id}`}
-                className="w-[200px] flex items-center justify-center bg-gray-700 rounded-xl px-2 sm:px-4 py-2"
-                key={genre.id}
-              >
+                className='w-[100px] sm:w-[200px] flex items-center justify-center bg-gray-700 rounded-xl px-0 sm:px-4 py-2 '
+                key={genre.id}>
                 {genre.name}
               </NavLink>
             ))}
           </div>
-        </>
+        </div>
       )}
     </>
   );
